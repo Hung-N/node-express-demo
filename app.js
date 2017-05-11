cbf790e47b2d85d19946630d9a58ed510c2b32fe
Hung Nguyen <reachhungnguyen@gmail.com>
2017-05-11 10:55:46 -0700

    Create logger middleware

--- app.js
+++ app.js
@@ -13,14 +13,22 @@
 
  const app = express();
 
+// unlike app.get, app.use will work for all HTTP verbs, if we do not give a URL (string) for the first argument, it will match for every URL
+ app.use((request, response, next) => {
+   console.log(`${request.method} - ${request.path} - ${new Date().toString()}`)
+   // next(); is a callback function and third argument of the middleware callback, tells express to move on the the next middleware
+   next();
+ })
 
-// when user visits 'http://localhost:4545/helloWorld VERB: Get' a call back function is called (request & response are objects)
+// when user visits 'http://localhost:4545/helloWorld (VERB: get)' a call back function is called (request & response are objects)
  app.get('/helloWorld', (request, response) => {
-   debugger;
+
    // this callback (which receives a request & response) is usually named `Middleware`
    // the arguments passed to this callback are in order: request, response, next
    // - request: an object that contains the entire message from the client (usually a browser)
    // - response: an object that contains the message our server will reply with back to the client
+
+
 
    response.send('Yo!');
  });
